90744290ac0970772881f4bea005f00162f5ea06
Dinesh Reddy <dineshmaareddy@gmail.com>
2021-04-10 17:09:56 +0530

    first commit

---
+++ 12-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const server = http.createServer((req, res)=>{
+    // console.log(req)
+    if(req.url === '/'){
+        res.end('Welcome to our home page')
+    }
+    // res.write('Welcome to our home page')
+    // res.end()
+     else if(req.url === '/about'){
+        res.end("Here is our short history")
+    }
+    else res.end(`<h1>Oops!</h1> <p>Can't seem to find the page you are looking for</p> <a href='/'>go home</a> `)
+})
+
+server.listen(5000);
